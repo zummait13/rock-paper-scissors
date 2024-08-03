37d5f4281e6696e496a9db8f7c80e4fa562d02dc
zummait13 <34415872+zummait13@users.noreply.github.com>
2024-08-03 23:54:07 +0300

    Add the computer choice generation and test probability

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 // Test connection
 console.log("Hello, it's test of connection")
+
+// GET THE COMPUTER CHOICE
+// Function getComputerChoice generating paper || rock || scissors
+function getComputerChoice () {
+    // Initialize natural randomNumber from 0 to 2
+    let randomNumber = getRandomNumber();
+
+    // Match it to Choice
+    let computerChoice = (randomNumber == 0) ? 'paper' 
+        : (randomNumber == 1) ? 'rock' 
+        : 'scissors';
+
+        return computerChoice;
+}
+
+// Fucntion: getRandomNumber: get random number from 0 to 2
+// Math.random() * max
+// Round it
+function getRandomNumber () {
+    return Math.floor(Math.random() * 3);
+}
+
+// variable memorizing the computerChoice
+let computerChoice = getComputerChoice();
+
+// Log the answer
+console.log(computerChoice);
+
+// TEST PROBABILITY
+
+// declare counters for each choice
+// let paperCounter = 0;
+// let rockCounter = 0;
+// let scissorsCounter = 0;
+
+// for (let i = 0; i <= 100; i++) {
+    // let computerChoice = getComputerChoice();
+    // condition-comparison
+    // if (computerChoice == 'paper') { paperCounter++; }
+    // else if (computerChoice == 'rock') {rockCounter++; }
+    // else scissorsCounter++;
+    // count each choice
+// }
+
+// Log the probability
+// console.log(paperCounter);
+// console.log(rockCounter);
+// console.log(scissorsCounter);
